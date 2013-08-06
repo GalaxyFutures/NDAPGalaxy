375ba76f231c91481d67b22c8f5ebd251d2bd401
norsd <norsd@163.com>
2013-08-06 13:05:07 +0800

    add Test_Price_YTM.R

---
+++ Test_Price_YTM.R
@@ -0,0 +1,7 @@
+BondInfo = ResetToday(BondInfo,"GOV","2012-12-1",FALSE,FALSE,TRUE)
+BondInfo = InitBondPrice(BondInfo,"GOV",QuoteBond)
+ytms = c(rep(0,length(BondInfo$GOV$ISIN)))
+
+names(ytms) = c(BondInfo$GOV$ISIN)
+
+prices=BondYTM2PRICE(BondInfo,"GOV",ytms)
