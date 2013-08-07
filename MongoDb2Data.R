e8424a08262a8cdc974f23b4365213442606c77a
norsd <norsd2004@gmail.com>
2013-08-07 18:14:16 +0800

    COMMIT

--- MongoDb2Data.R
+++ MongoDb2Data.R
@@ -7,7 +7,7 @@ InitMongoDb = function(arg_strAddress,arg_strUser,arg_strPass)
   ndapdb <<- mongo.create(host=arg_strAddress,username=arg_strUser,password=arg_strPass, db="NDAP")
 }
 
-InitNDAPMongoDb =function() 
+InitNDAPDb =function() 
 {
   InitMongoDb("221.133.243.54:3401","NDAPReader","Reader@Galaxy")
 }
