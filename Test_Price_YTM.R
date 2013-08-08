9d24923090c8ce52931fcbe1f92a5c71f1b9e0e8
wenfushan <wenfushan.wfs@gmail.com>
2013-08-08 10:24:18 +0800

    commit

--- Test_Price_YTM.R
+++ Test_Price_YTM.R
@@ -1,16 +1,39 @@
-QuoteBond <- list()
-QuoteBond[['070010.IB']]$date=c(as.Date("2012-12-10"))
-QuoteBond[['070010.IB']]$priceDirty=c(106.7395)
-QuoteBond[['070010.IB']]$accruedInterest=c(1.5671)
-QuoteBond[['070010.IB']]$priceClean=c(105.1724)
-QuoteBond[['070010.IB']]$YTM=c(10)#c(3.1928)
-QuoteBond[['070010.IB']]$duration=c(4.0728)
-QuoteBond[['070010.IB']]$convexity=c(21.6808)
-BondInfo = ResetToday(BondInfo,"GOV","2012-12-10",FALSE,FALSE,TRUE)
-BondInfo = InitBondPrice(BondInfo,"GOV",QuoteBond)
 
-ytms = c(rep(0,length(BondInfo$GOV$ISIN)))
-names(ytms) = c(BondInfo$GOV$ISIN)
-ytms[1]=3
-prices=BondYTM2PRICE(BondInfo,"GOV",ytms)
-prices
+
+###########################################################################################################
+#已知ytm，求price，duration，convexity
+#couponRate,ytm单位%
+#求price用函数Bondytm2price
+#price = 99.99278
+
+couponRate = 3
+issueDate = "2010/5/1"
+endDate = "2014/5/1"
+freq = 1
+today = "2013/7/15"
+ytm = 3
+price = Bondytm2price(couponRate,issueDate,endDate,freq,today,ytm)[1]
+print(price)
+#求Duration和Convexity用函数Bondytm2DurationConvexity
+Duration = Bondytm2DurationConvexity(couponRate,issueDate,endDate,freq,today,ytm)[1]
+Convexity = Bondytm2DurationConvexity(couponRate,issueDate,endDate,freq,today,ytm)[2]
+print(Duration)
+print(Convexity)
+
+###########################################################################################################
+#已知price，求ytm，duration，convexity
+#couponRate,ytm单位%
+#求ytm用函数Bondprice2ytm
+
+price = 99.99278
+ytm = Bondprice2ytm(couponRate,issueDate,endDate,freq,today,price)
+print(ytm)
+#求Duration和Convexity用函数Bondytm2DurationConvexity,上面求出的ytm作为参数
+Duration = Bondytm2DurationConvexity(couponRate,issueDate,endDate,freq,today,ytm)[1]
+Convexity = Bondytm2DurationConvexity(couponRate,issueDate,endDate,freq,today,ytm)[2]
+print(Duration)
+print(Convexity)
+
+
+
+
