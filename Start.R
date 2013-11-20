b93c0183448936671694d31d3bbfc253b333143d
norsd <norsd@163.com>
2013-11-20 14:14:30 +0800

    aaa

--- Start.R
+++ Start.R
@@ -3,11 +3,13 @@ ChineseOS = False
 
 if(ChineseOS)
 {
-  source("CSharpR.R",encoding="utf-8")
-  source("MongoHelper.R",encoding="utf-8")
-  source("MongoDb2Data.R",encoding="utf-8")
+  #source("CSharpR.R",encoding="utf-8")
+  #source("MongoHelper.R",encoding="utf-8")
+  #source("MongoDb2Data.R",encoding="utf-8")
   source("HELPER.R",encoding="utf-8")
-  source("MyData.R",encoding="utf-8")
+  #source("MyData.R",encoding="utf-8")
+  source("CTDScenario.R",encoding="utf-8")
+  source("CalculateRepoRelated.R",encoding="utf-8")
 }
 else
 {
@@ -15,6 +17,7 @@ else
   #source("MongoHelper.R")
   #source("MongoDb2Data.R")
   source("HELPER.R")
+  source("CTDScenario.R")
   #source("MyData.R")
   source("CalculateRepoRelated.R")
 }
